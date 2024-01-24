132a45e9134a9fce7b239ab16e0fe1edbeaefd7d
Starciad <davigamerlsf16@gmail.com>
2024-01-24 13:50:18 -0300

    feat: Add 'getRandomArrayElement' function to random.js.

--- scripts/math/random.js
+++ scripts/math/random.js
@@ -2,4 +2,8 @@ export const random = Object.freeze({
     getRandomNumber: function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     },
+
+    getRandomArrayElement: function (array) {
+        return array[this.getRandomNumber(0, array.length - 1)];
+    },
 });
