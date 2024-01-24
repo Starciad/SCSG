49116fd32f823df8359e2e4abfb7920c2a976faa
Starciad <davigamerlsf16@gmail.com>
2024-01-24 14:43:22 -0300

    feat: Add collection for blood types

---
+++ scripts/collections/BloodTypeCollection.js
@@ -0,0 +1,14 @@
+import { random } from "../math/random.js";
+
+export const BloodTypeCollection = Object.freeze({
+    bloodTypes: ["A", "B", "AB", "O"],
+    rhFactors: ["+", "-"],
+
+    getRandomBloodType: function () {
+        return random.getRandomArrayElement(this.bloodTypes);
+    },
+
+    getRandomRhFactor: function () {
+        return random.getRandomArrayElement(this.rhFactors);
+    },
+});
