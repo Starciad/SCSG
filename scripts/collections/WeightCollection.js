bc1172551ee695b7ef0104632cd84dcc83c8d0d8
Starciad <davigamerlsf16@gmail.com>
2024-01-24 14:44:17 -0300

    feat: Add collection for weights.

---
+++ scripts/collections/WeightCollection.js
@@ -0,0 +1,18 @@
+import { random } from "../math/random.js";
+
+export const WeightCollection = Object.freeze({
+    weightRangeBasedOnAge: [
+        { minAge: 0, maxAge: 12, minWeight: 20, maxWeight: 40 },
+        { minAge: 13, maxAge: 18, minWeight: 45, maxWeight: 70 },
+        { minAge: 19, maxAge: 30, minWeight: 50, maxWeight: 80 },
+        { minAge: 31, maxAge: 50, minWeight: 45, maxWeight: 75 },
+        { minAge: 51, maxAge: 100, minWeight: 40, maxWeight: 70 }
+    ],
+
+    getRandomWeight: function (age) {
+        const currentRange = this.weightRangeBasedOnAge.find(range => age >= range.minAge && age <= range.maxAge);
+        const randomWeight = random.getRandomNumber(currentRange.minWeight, currentRange.maxWeight);
+        
+        return randomWeight;
+    },
+});
